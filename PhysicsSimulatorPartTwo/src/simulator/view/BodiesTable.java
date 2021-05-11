67343de4541d6af429591e58ef5b1ad7ad535c4c
Jan Majchrzycki <majchrzycki2000@gmail.com>
2021-05-11 13:55:09 +0200

    6.2 probably done. waiting for samir's confirmation. added viewer from 6.3 but idk some things

--- PhysicsSimulatorPartTwo/src/simulator/view/BodiesTable.java
+++ PhysicsSimulatorPartTwo/src/simulator/view/BodiesTable.java
@@ -5,18 +5,28 @@ import java.awt.Color;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.TitledBorder;
 
 import simulator.control.Controller;
 
 public class BodiesTable extends JPanel {
+	
 BodiesTable(Controller ctrl) {
 setLayout(new BorderLayout());
 setBorder(BorderFactory.createTitledBorder(
 BorderFactory.createLineBorder(Color.black, 2),
 "Bodies",
 TitledBorder.LEFT, TitledBorder.TOP));
-// TODO complete
+
+BodiesTableModel model = new BodiesTableModel(ctrl);
+JTable table = new JTable(model);
+JScrollPane jsp = new JScrollPane(table);
+add(jsp);
+
+
 }
+
 }
 
--- PhysicsSimulatorPartTwo/src/simulator/view/BodiesTableModel.java
+++ PhysicsSimulatorPartTwo/src/simulator/view/BodiesTableModel.java
@@ -4,63 +4,84 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
-
 import simulator.control.Controller;
 import simulator.model.Body;
 import simulator.model.SimulatorObserver;
 
-public class BodiesTableModel extends AbstractTableModel
-implements SimulatorObserver {
-// ...
+public class BodiesTableModel extends AbstractTableModel implements SimulatorObserver {
+
 private List<Body> _bodies;
+private String[] _colNames = { "Id", "Mass", "Position", "Velocity", "Force" };
+
 BodiesTableModel(Controller ctrl) {
 _bodies = new ArrayList<>();
 ctrl.addObserver(this);
 }
+
 @Override
 public int getRowCount() {
 	return _bodies == null ? 0 : _bodies.size();
 }
+
 @Override
 public int getColumnCount() {
-	int num = 0;
-	
-	return num;
-	// TODO complete
+	return _colNames.length;
 }
+
 @Override
-public String getColumnName(int column) {
-	String name = null;
-	
-	return name;
-// TODO complete
+public String getColumnName(int col) {
+	return _colNames[col];
 }
 
 @Override
 public Object getValueAt(int rowIndex, int columnIndex) {
-	return columnIndex;
-// TODO complete
+	Object s = null;
+	switch (columnIndex) {
+	case 0:
+		s = _bodies.get(rowIndex).getId();
+		break;
+	case 1:
+		s = _bodies.get(rowIndex).getMass();
+		break;
+	case 2:
+		s = _bodies.get(rowIndex).getPosition();
+		break;
+	case 3:
+		s = _bodies.get(rowIndex).getVelocity();
+		break;
+	case 4:
+		s = _bodies.get(rowIndex).getForce();
+		break;
+	}
+	return s;
 }
-// SimulatorObserver methods
-// ...
+
+public void update() {
+	fireTableStructureChanged();
+}
+
 @Override
 public void onRegister(List<Body> bodies, double time, double dt, String fLawsDesc) {
-
+	_bodies = bodies;
+	update();
 	
 }
 @Override
 public void onReset(List<Body> bodies, double time, double dt, String fLawsDesc) {
-	// TODO Auto-generated method stub
+	_bodies = bodies;
+	update();
 	
 }
 @Override
 public void onBodyAdded(List<Body> bodies, Body b) {
-	// TODO Auto-generated method stub
+	_bodies = bodies;
+	update();
 	
 }
 @Override
 public void onAdvance(List<Body> bodies, double time) {
-	// TODO Auto-generated method stub
+	_bodies = bodies;
+	update();
 	
 }
 @Override
@@ -73,5 +94,6 @@ public void onForceLawsChanged(String fLawsDesc) {
 	// TODO Auto-generated method stub
 	
 }
+
 }
 
